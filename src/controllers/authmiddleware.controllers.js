2ce55a029f599b6d4b370b9b4580dc64404d6a81
Mariomin23 <mario.minuesa@gmail.com>
2026-05-04 16:57:05 +0200

    Add example environment variables and admin middleware template

---
+++ src/controllers/authmiddleware.controllers.js
@@ -0,0 +1,9 @@
+    /*export const isAdmin =(req,res,next)=>{
+    if(req.user&&req.user.role==='admin'){
+        console.log("Eres Admin, puedes pasar")
+        next();
+    } else {
+    res.status(403).json({message:"acceso denegado. No eres admin"})
+    }
+
+    };*/
