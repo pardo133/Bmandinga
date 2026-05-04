e513056b5a4c9723d2724cf60b9f441e761ef1cc
Mariomin23 <mario.minuesa@gmail.com>
2026-05-04 13:27:42 +0200

    defaultuser

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
   correo: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String,enum: ['user', 'admin'], default:'user' }
 });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
