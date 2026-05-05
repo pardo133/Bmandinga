31a91831c48d8fb5c1b4237bf656fb596cbb2c23
pardo133 <pardo133@hotmail.com>
2026-05-05 16:05:26 +0200

    cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connectDB } from './src/config/db.js';
 import userRoutes from './src/routes/user.routes.js';
 import productRoutes from "./src/routes/product.routes.js";
 import paymentRoutes from './src/routes/payment.routes.js';
-
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use("/api/products", productRoutes);
 app.use('/api/payments', paymentRoutes);
+
+
+    app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor en http://localhost:${PORT}`);
