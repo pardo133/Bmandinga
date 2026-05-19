9cbaaf68287256e31a7cbf34f2c6a0cdad4846e8
pardo133 <pardo133@hotmail.com>
2026-05-19 10:47:52 +0200

    final

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -19,9 +19,3 @@ export function authMiddleware(req, res, next) {
     next();
 }
 
-export function adminMiddleware(req, res, next) {
-    if (req.user?.role !== 'admin') {
-        return res.status(403).json({ mensaje: "Acceso denegado: No eres admin" });
-    }
-    next();
-}
